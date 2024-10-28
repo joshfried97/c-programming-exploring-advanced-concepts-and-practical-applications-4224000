5c2b3bc63f39e4a8f968101d720985162cdee470
joshfried97 <joshfried97@gmail.com>
2024-10-28 10:21:08 +0000

    Chapter 3 done

--- Ch03/03_04/03_04-challenge1.c
+++ Ch03/03_04/03_04-challenge1.c
@@ -8,5 +8,42 @@
 
 int main()
 {
+	// Emoji array: sad, happy, meh and confused
+	wchar_t faces[4] = {0x2639, 0x1F60A, 0x1F610, 0x1F914};
+
+	const int count = 32;
+	wchar_t input[count];
+
+	setlocale(LC_ALL, "en_US.UTF-8");
+
+	// Display options
+	wprintf(L"What is your mood?\n");
+	wprintf(L"1:%lc sad\n", faces[0]);
+	wprintf(L"2:%lc happy\n", faces[1]);
+	wprintf(L"3:%lc meh\n", faces[2]);
+
+	// Read in option
+	int n;
+	wscanf(L"%d", &n);
+
+	switch (n)
+	{
+	case 1:
+		wprintf(L"%lc Sad\n", faces[0]);
+		break;
+
+	case 2:
+		wprintf(L"%lc Happy\n", faces[1]);
+		break;
+
+	case 3:
+		wprintf(L"%lc Meh\n", faces[2]);
+		break;
+
+	default:
+		wprintf(L"%lc Huh?\n", faces[3]);
+		break;
+	}
+
 	return 0;
 }
